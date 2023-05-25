7783d694cdb749edb49d997009388ce7b5a92b55
Keith Senti <ksenti@gmail.com>
2023-05-24 23:52:42 -0500

    Separate cpp files. More stable charging for 1860M battery.

--- listener_clients/m5stickc-listener/m5stickc-listener.ino
+++ listener_clients/m5stickc-listener/m5stickc-listener.ino
@@ -3,6 +3,7 @@
 #include "CustomWifi.h"
 #include "Screens.h"
 #include <PinButton.h>
+#include <millisDelay.h>
 
 #define TRIGGER_PIN 0   //reset pin 
 
@@ -10,6 +11,26 @@
 PinButton btnM5(37);      //the "M5" button on the device
 PinButton btnAction(39);  //the "Action" button on the device
 uint8_t wasPressed();
+
+millisDelay oneSecDelay;
+
+int currentBrightness = 9;
+int maxBrightness = 12;
+
+String powerMode = "";
+String batWarningLevel = "";
+float batVoltage = 0;
+float batPercentage = 0;
+float batCurrent = 0;
+float batChargeCurrent = 0;
+float vbusVoltage = 0;
+float vinVoltage = 0;
+int chargeCurrent = 100;
+
+const int batPercentage_C = 60;
+int batPercentage_I = 0;
+float batPercentage_M = 0;
+float batPercentage_A [batPercentage_C];
 
 
 // Start Setup
@@ -29,6 +50,9 @@ void setup() {
   listenerDeviceName = "m5StickC-" + String((uint16_t)(chipid>>32)) + String((uint32_t)chipid);
 
   M5.begin();
+
+  currentBrightness = 9;
+  M5.Axp.ScreenBreath(currentBrightness);
   M5.Lcd.setRotation(3);
   M5.Lcd.setCursor(0, 20);
   M5.Lcd.fillScreen(TFT_BLACK);
@@ -39,6 +63,12 @@ void setup() {
   logger("Tally Arbiter M5StickC+ Listener Client booting.", "info");
   logger("Listener device name: " + listenerDeviceName, "info");
 
+  // For power screen
+  tftSprite.createSprite(240, 135); // Create a 240x135 canvas.
+  tftSprite.setRotation(3);
+  M5.Axp.EnableCoulombcounter();    // Enable Coulomb counter.
+
+  // Load preferences
   preferences.begin("tally-arbiter", false);
 
   // added to clear out corrupt prefs
@@ -79,8 +109,13 @@ void setup() {
     pinMode(led_aux, OUTPUT);
     digitalWrite(led_aux, LOW);
   #endif
-  
+
+  oneSecDelay.start(1000);  // start delay
   connectToServer();
+
+  delay(3000);
+  M5.Lcd.fillScreen(TFT_BLACK);
+  showTally();
 
 }
 
@@ -98,21 +133,39 @@ void loop() {
   btnAction.update();
 
   if (btnM5.isClick()) {
+    
+    M5.Lcd.fillScreen(TFT_BLACK);
+    currentScreen = currentScreen + 1;
+    if (currentScreen > 2) {
+      currentScreen = 0;
+    }
+    
     switch (currentScreen) {
       case 0:
-        showSettings();
-        currentScreen = 1;
+        showTally();
         break;
       case 1:
-        showTally();
-        currentScreen = 0;
+        showSettings();
         break;
+      case 2:
+        showPowerInfo();
+        break;
+    }
+
+  }
+
+  if (oneSecDelay.justFinished()) {
+    oneSecDelay.repeat();
+    doPowerManagement();
+    if (currentScreen == 2) {
+      showPowerInfo();
     }
   }
 
   if (btnAction.isClick()) {
     updateBrightness();
   }
+
 }
 
 
@@ -155,6 +208,109 @@ void checkReset() {
       }
     }
   }
+}
+
+
+void updateBrightness() {
+  if(currentBrightness >= maxBrightness) {
+    currentBrightness = 7;
+  } else {
+    currentBrightness++;
+  }
+  M5.Axp.ScreenBreath(currentBrightness);
+}
+
+
+void doPowerManagement() {
+  
+  batWarningLevel = (M5.Axp.GetWarningLevel()) ? "LOW BATTERY" : "";
+  batVoltage = M5.Axp.GetBatVoltage();
+
+  float batPercentage1 = (batVoltage - 3.0) / (4.2-3.0) * 100; // min = 3.0 max = 4.2
+  float batPercentage2 = (batPercentage1 <= 100) ? batPercentage1 : 100;
+  batPercentage = batPercentage2;
+
+  if (batPercentage_A[batPercentage_I] == batPercentage_M) {
+    batPercentage_M = 0;
+  }
+
+  batPercentage_A[batPercentage_I] = batPercentage2;
+  batPercentage_I = batPercentage_I + 1;
+  if (batPercentage_I > batPercentage_C) {
+    batPercentage_I = 0;
+  }
+
+  if (batPercentage > batPercentage_M) {
+    for (int i = 0; i <= batPercentage_C; ++i) {
+      if (batPercentage_A[i] > batPercentage_M) {
+        batPercentage_M = batPercentage_A[i];
+      }
+    }
+  }
+  
+
+  batCurrent = M5.Axp.GetBatCurrent();
+  batChargeCurrent = M5.Axp.GetBatChargeCurrent();
+  vbusVoltage = M5.Axp.GetVBusVoltage();
+  vinVoltage = M5.Axp.GetVinVoltage();
+
+
+  // Power Mode
+  if (vinVoltage > 3.8) {         // 5v IN Charge
+    
+    maxBrightness = 12;
+    esp_wifi_set_ps(WIFI_PS_MIN_MODEM);  //WIFI_PS_NONE
+
+    if ((batPercentage_M < 80) && (chargeCurrent != 780)) {
+      powerMode = "Fast Charge";
+      chargeCurrent = 780;
+      M5.Axp.Write1Byte(0x33, 0xc8);
+    } else if (batPercentage_M >= 80 && batPercentage_M < 90 && chargeCurrent != 280) {
+      powerMode = "Performance";
+      chargeCurrent = 280;
+      M5.Axp.Write1Byte(0x33, 0xc2);
+    } else if (batPercentage_M >= 90 && chargeCurrent != 190) {
+      powerMode = "Performance";
+      chargeCurrent = 190;
+      M5.Axp.Write1Byte(0x33, 0xc1);
+    }
+
+  } else if (vbusVoltage > 3.8) {   // 5v USB Charge
+
+    powerMode = "Performance";
+    maxBrightness = 12;
+    esp_wifi_set_ps(WIFI_PS_MIN_MODEM);
+    
+    if (chargeCurrent != 100) {
+      chargeCurrent = 100;
+      M5.Axp.Write1Byte(0x33, 0xc0);
+    }
+
+  } else {
+    
+    // 3v Battery
+
+    if (chargeCurrent != 100) {
+      chargeCurrent = 100;
+      M5.Axp.Write1Byte(0x33, 0xc0);
+    }
+
+    if (batPercentage_M >= 80) {
+      powerMode = "Balanced";
+      maxBrightness = 12;
+      esp_wifi_set_ps(WIFI_PS_MIN_MODEM);
+    } else {
+      powerMode = "Power Saver";
+      maxBrightness = 9;
+      if (currentBrightness > maxBrightness) {
+        currentBrightness = maxBrightness;
+        M5.Axp.ScreenBreath(currentBrightness);
+      }
+      esp_wifi_set_ps(WIFI_PS_MAX_MODEM);
+    }
+
+  }
+
 }
 
 
